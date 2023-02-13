ddcd558df8f9c58bb457e29ca102914a993275c5
Bagus Andrian <babagusandrian@gmail.com>
2023-02-13 16:35:07 +0700

    add unit test

--- hitratemechanism_test.go
+++ hitratemechanism_test.go
@@ -1,77 +1,126 @@
 package HitRateMechanism
 
 import (
+	"context"
 	"fmt"
+	"reflect"
 	"testing"
 	"time"
 
 	"github.com/alicebob/miniredis"
+	// "github.com/garyburd/redigo/redis"
+	"github.com/redis/go-redis/v9"
 )
 
 func BenchmarkCustomHitRate(b *testing.B) {
-	for i := 0; i <= 1000; i++ {
+	for i := 0; i <= 100; i++ {
 		prefix := fmt.Sprintf("prefix+%d", i)
 		keyCheck := fmt.Sprintf("keycheck+%d", i)
-		miniRds, err := miniredis.Run()
-		if err != nil {
-			b.Errorf("failed run miniRds err: %+v\n", err)
-		}
-		defer miniRds.Close()
-
-		redisSrv, err := miniredis.Run()
-		if err != nil {
-			b.Errorf("failed run redisSrv err: %+v\n", err)
-		}
-		defer redisSrv.Close()
-		New("local", redisSrv.Addr(), "tcp")
+		rdb := redis.NewClient(&redis.Options{
+			Addr:     "localhost:6379",
+			Password: "", // no password set
+			DB:       0,  // use default DB
+		})
+		ctx := context.Background()
+		New("local", "localhost:6379", "tcp")
 		prefixKeycheck := fmt.Sprintf("%s-%s", prefix, keyCheck)
-		_, err = redisSrv.HIncr(prefixKeycheck, "count", i*100)
+		_, err := rdb.HIncrBy(ctx, prefixKeycheck, "count", int64(i*100)).Result()
 		if err != nil {
 			b.Errorf("got errof for incr data mock err:%+v\n", err)
 		}
-		redisSrv.SetTTL(prefixKeycheck, 10*time.Second)
-		redisSrv.HSet(keyCheck, "field", "value")
-		redisSrv.SetTTL(keyCheck, 10*time.Second)
+		rdb.Expire(ctx, prefixKeycheck, 10*time.Second)
+		rdb.HSet(ctx, keyCheck, "field", "value")
+		rdb.Expire(ctx, keyCheck, 10*time.Second)
 		b.Run(fmt.Sprintf("benchmark CustomHitRate %d", i), func(b *testing.B) {
-			Pool.CustomHitRate("local", prefix, keyCheck)
+			req := ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:       "local",
+					ExtendTTLKey:      60,
+					ExtendTTLKeyCheck: 60,
+					ParseLayoutTime:   "2006-01-02 15:04:05 Z0700 MST",
+				},
+				Threshold: ThresholdCustomHitrate{
+					LimitMaxTTL:         300,
+					MaxRPS:              20,
+					LimitExtendTTLCheck: 60,
+				},
+				AttributeKey: AttributeKeyhitrate{
+					KeyCheck: "keycheck",
+					Prefix:   "prefix",
+				},
+			}
+			Pool.CustomHitRate(req)
 		})
 	}
 }
 func TestCustomHitRate(t *testing.T) {
-	type args struct {
-		dbname, prefix, keyCheck string
-	}
 	tests := []struct {
 		name            string
-		args            args
+		args            ReqCustomHitRate
 		wantErr         bool
 		preparationData func(mockRedis *miniredis.Miniredis)
 	}{
 		{
 			name: "Positive CustomHitRate",
-			args: args{
-				dbname:   "local",
-				prefix:   "prefix",
-				keyCheck: "keycheck",
+			args: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:       "local",
+					ExtendTTLKey:      60,
+					ExtendTTLKeyCheck: 30,
+					ParseLayoutTime:   "2006-01-02 15:04:05 Z0700 MST",
+				},
+				Threshold: ThresholdCustomHitrate{
+					LimitMaxTTL:         300,
+					MaxRPS:              20,
+					LimitExtendTTLCheck: 60,
+				},
+				AttributeKey: AttributeKeyhitrate{
+					KeyCheck: "keycheck",
+					Prefix:   "prefix",
+				},
 			},
 			preparationData: func(mockRedis *miniredis.Miniredis) {},
 		},
 		{
 			name: "Negative CustomHitRate",
-			args: args{
-				dbname:   "wrongDB",
-				prefix:   "prefix",
-				keyCheck: "keycheck",
+			args: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:       "wrongDB",
+					ExtendTTLKey:      60,
+					ExtendTTLKeyCheck: 30,
+					ParseLayoutTime:   "2006-01-02 15:04:05 Z0700 MST",
+				},
+				Threshold: ThresholdCustomHitrate{
+					LimitMaxTTL:         300,
+					MaxRPS:              20,
+					LimitExtendTTLCheck: 60,
+				},
+				AttributeKey: AttributeKeyhitrate{
+					KeyCheck: "keycheck",
+					Prefix:   "prefix",
+				},
 			},
 			preparationData: func(mockRedis *miniredis.Miniredis) {},
 			wantErr:         true,
 		},
 		{
 			name: "CustomHitRate->hitRateGetData low RPS",
-			args: args{
-				dbname:   "local",
-				prefix:   "prefix",
-				keyCheck: "keycheck",
+			args: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:       "local",
+					ExtendTTLKey:      60,
+					ExtendTTLKeyCheck: 30,
+					ParseLayoutTime:   "2006-01-02 15:04:05 Z0700 MST",
+				},
+				Threshold: ThresholdCustomHitrate{
+					LimitMaxTTL:         300,
+					MaxRPS:              20,
+					LimitExtendTTLCheck: 60,
+				},
+				AttributeKey: AttributeKeyhitrate{
+					KeyCheck: "keycheck",
+					Prefix:   "prefix",
+				},
 			},
 			wantErr: false,
 			preparationData: func(mockRedis *miniredis.Miniredis) {
@@ -86,10 +135,22 @@ func TestCustomHitRate(t *testing.T) {
 		},
 		{
 			name: "CustomHitRate->hitRateGetData High RPS",
-			args: args{
-				dbname:   "local",
-				prefix:   "prefix",
-				keyCheck: "keycheck",
+			args: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:       "local",
+					ExtendTTLKey:      60,
+					ExtendTTLKeyCheck: 30,
+					ParseLayoutTime:   "2006-01-02 15:04:05 Z0700 MST",
+				},
+				Threshold: ThresholdCustomHitrate{
+					LimitMaxTTL:         300,
+					MaxRPS:              20,
+					LimitExtendTTLCheck: 60,
+				},
+				AttributeKey: AttributeKeyhitrate{
+					KeyCheck: "keycheck",
+					Prefix:   "prefix",
+				},
 			},
 			wantErr: false,
 			preparationData: func(mockRedis *miniredis.Miniredis) {
@@ -104,10 +165,22 @@ func TestCustomHitRate(t *testing.T) {
 		},
 		{
 			name: "CustomHitRate->hitRateGetData High RPS new TTL < end_time",
-			args: args{
-				dbname:   "local",
-				prefix:   "prefix",
-				keyCheck: "keycheck",
+			args: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:       "local",
+					ExtendTTLKey:      60,
+					ExtendTTLKeyCheck: 30,
+					ParseLayoutTime:   "2006-01-02 15:04:05 Z0700 MST",
+				},
+				Threshold: ThresholdCustomHitrate{
+					LimitMaxTTL:         300,
+					MaxRPS:              20,
+					LimitExtendTTLCheck: 60,
+				},
+				AttributeKey: AttributeKeyhitrate{
+					KeyCheck: "keycheck",
+					Prefix:   "prefix",
+				},
 			},
 			wantErr: false,
 			preparationData: func(mockRedis *miniredis.Miniredis) {
@@ -137,12 +210,12 @@ func TestCustomHitRate(t *testing.T) {
 				if err != nil {
 					t.Errorf("failed run redisSrv err: %+v\n", err)
 				}
-				New(tt.args.dbname, redisSrv.Addr(), "tcp")
+				New(tt.args.Config.RedisDBName, redisSrv.Addr(), "tcp")
 				tt.preparationData(redisSrv)
 			}
-			_, _, err := Pool.CustomHitRate(tt.args.dbname, tt.args.prefix, tt.args.keyCheck)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("wantErr: %t got error CustomHitRate err: %+v\n", tt.wantErr, err)
+			resp := Pool.CustomHitRate(tt.args)
+			if (resp.Err != nil) != tt.wantErr {
+				t.Errorf("wantErr: %t got error CustomHitRate err: %+v\n", tt.wantErr, resp.Err)
 			}
 		})
 	}
@@ -389,4 +462,191 @@ func TestCalculateNewTTL(t *testing.T) {
 		})
 	}
 
+}
+
+func TestValidateReqCustomHitRate(t *testing.T) {
+	tests := []struct {
+		name    string
+		args    ReqCustomHitRate
+		output  ReqCustomHitRate
+		wantErr bool
+		err     error
+	}{
+		{
+			name: "positive case",
+			args: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:       "local",
+					ExtendTTLKey:      60,
+					ExtendTTLKeyCheck: 60,
+					ParseLayoutTime:   "2006-01-02 15:04:05 Z0700 MST",
+				},
+				Threshold: ThresholdCustomHitrate{
+					LimitMaxTTL:         300,
+					MaxRPS:              20,
+					LimitExtendTTLCheck: 60,
+				},
+				AttributeKey: AttributeKeyhitrate{
+					KeyCheck: "keycheck",
+					Prefix:   "prefix",
+				},
+			},
+			output: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:       "local",
+					ExtendTTLKey:      60,
+					ExtendTTLKeyCheck: 60,
+					ParseLayoutTime:   "2006-01-02 15:04:05 Z0700 MST",
+				},
+				Threshold: ThresholdCustomHitrate{
+					LimitMaxTTL:         300,
+					MaxRPS:              20,
+					LimitExtendTTLCheck: 60,
+				},
+				AttributeKey: AttributeKeyhitrate{
+					KeyCheck: "keycheck",
+					Prefix:   "prefix",
+				},
+			},
+			wantErr: false,
+			err:     nil,
+		},
+		{
+			name:    "empty db name",
+			args:    ReqCustomHitRate{},
+			output:  ReqCustomHitRate{},
+			wantErr: true,
+			err:     fmt.Errorf("empty redisDBName config"),
+		},
+		{
+			name: "empty db ParseLayoutTime",
+			args: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName: "testing",
+				},
+			},
+			output: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName: "testing",
+				},
+			},
+			wantErr: true,
+			err:     fmt.Errorf("empty layout format for parse time"),
+		},
+		{
+			name: "empty maxRPS",
+			args: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:     "testing",
+					ParseLayoutTime: "parse layout",
+				},
+			},
+			output: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:     "testing",
+					ParseLayoutTime: "parse layout",
+				},
+			},
+			wantErr: true,
+			err:     fmt.Errorf("empty threshold maxRPS"),
+		},
+		{
+			name: "empty key check",
+			args: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:     "testing",
+					ParseLayoutTime: "parse layout",
+				},
+				Threshold: ThresholdCustomHitrate{
+					MaxRPS: 20,
+				},
+			},
+			output: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:     "testing",
+					ParseLayoutTime: "parse layout",
+				},
+				Threshold: ThresholdCustomHitrate{
+					MaxRPS: 20,
+				},
+			},
+			wantErr: true,
+			err:     fmt.Errorf("empty key check"),
+		},
+		{
+			name: "empty prefix",
+			args: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:     "testing",
+					ParseLayoutTime: "parse layout",
+				},
+				Threshold: ThresholdCustomHitrate{
+					MaxRPS: 20,
+				},
+				AttributeKey: AttributeKeyhitrate{
+					KeyCheck: "keycheck",
+				},
+			},
+			output: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:     "testing",
+					ParseLayoutTime: "parse layout",
+				},
+				Threshold: ThresholdCustomHitrate{
+					MaxRPS: 20,
+				},
+				AttributeKey: AttributeKeyhitrate{
+					KeyCheck: "keycheck",
+				},
+			},
+			wantErr: true,
+			err:     fmt.Errorf("empty prefix"),
+		},
+		{
+			name: "replacing default value",
+			args: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:     "testing",
+					ParseLayoutTime: "2006-01-02 15:04:05 Z0700 MST",
+				},
+				Threshold: ThresholdCustomHitrate{
+					MaxRPS: 20,
+				},
+				AttributeKey: AttributeKeyhitrate{
+					KeyCheck: "keycheck",
+					Prefix:   "prefix",
+				},
+			},
+			output: ReqCustomHitRate{
+				Config: ConfigCustomHitRate{
+					RedisDBName:       "testing",
+					ExtendTTLKey:      60,
+					ExtendTTLKeyCheck: 60,
+					ParseLayoutTime:   "2006-01-02 15:04:05 Z0700 MST",
+				},
+				Threshold: ThresholdCustomHitrate{
+					LimitMaxTTL:         300,
+					MaxRPS:              20,
+					LimitExtendTTLCheck: 30,
+				},
+				AttributeKey: AttributeKeyhitrate{
+					KeyCheck: "keycheck",
+					Prefix:   "prefix",
+				},
+			},
+			wantErr: false,
+			err:     nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := validateReqCustomHitRate(tt.args)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("wantErr: %t got error validateReqCustomHitRate err: %+v\n", tt.wantErr, err)
+			}
+			if !reflect.DeepEqual(result, tt.output) {
+				t.Errorf("result is not same with output. result %+v | output %+v\n", result, tt.output)
+			}
+		})
+	}
 }
